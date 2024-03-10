efbaa9772c50b2a366c7f9f3fb648363e9565344
Sean Liu <yuhsiang.liu@mail.utoronto.ca>
2024-03-10 16:00:34 -0400

    Making Model

--- starter_folder-main/scripts/04-model.R
+++ starter_folder-main/scripts/04-model.R
@@ -11,19 +11,20 @@
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
-
+library(dplyr)
 #### Read data ####
 analysis_data <- read_parquet("starter_folder-main/data/analysis_data/analysis_data.parquet")
 set.seed(853)
 
 ces2020_reduced <- 
   ces2020 |> 
-  slice_sample(n = 1000)
+  slice_sample(n = 10000)
 
+#### Model Data ####
 political_preferences <-
   stan_glm(
-    voted_for ~ employ + educ + race,
-    data = ces2020_reduced,
+    voted_for ~ employment_stat + education + race,
+    data = ces2020,
     family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = 
@@ -31,27 +32,13 @@ political_preferences <-
     seed = 853
   )
 
-saveRDS(
-  political_preferences,
-  file = "political_preferences.rds"
-)
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = voted_for ~ gender + education,
-    data = analysis_data,
-    family = binomial(link = "logit"),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+
 
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "starter_folder-main/models/first_model.rds"
+  political_preferences,
+  file = "political_preferences.rds"
 )
 
 
